efd27b9604998f06836e3c9bd8056b8c70438def
wonnx <75650764+wonnx@users.noreply.github.com>
2020-12-10 16:47:40 +0900

    Create 2231.cpp

---
+++ 2231.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define MAXN (int)1e6
+using namespace std;
+
+int solve[MAXN];
+
+int main()
+{
+	int n; cin >> n;
+	for (int i = 1; i <= MAXN; i++)
+	{
+		int t = i, r = i;
+		while (t)
+		{
+			r += (t % 10);
+			t /= 10;
+		}
+		if (solve[r])continue;
+		solve[r] = i;
+	}
+	cout << solve[n] << endl;
+}
